c0528a1a2ead8b97c04dd9303866462a951fce56
Timothy Yong <tyong_23@hotmail.com>
2015-04-22 19:16:26 -0400

    new audio files

--- pocketsphinx/sample_raw.sh
+++ pocketsphinx/sample_raw.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+#gst-launch-0.10 -v alsasrc ! "audio/x-raw-int, endianness=1234, signed=true, width=32, height=32, rate=44100, channels=2" ! queue . ! filesink location="$1"
 gst-launch-0.10 -v alsasrc ! "audio/x-raw-int, endianness=1234, signed=true, width=16, height=16, rate=16000, channels=1" ! queue . ! filesink location="$1"
